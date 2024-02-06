c1344189a5d268f76359fe70902992242d147ef4
Joe Simpson <josephsimpson94@googlemail.com>
2024-02-06 14:38:00 +0000

    contact page filled out, styled, global css tweaked

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Header from "@/app/components/Header";
 
 const ContactMe = () => {
-  return <div>ContactMe</div>;
+  return (
+    <>
+      <Header />
+      <div className="contactOptions">
+        <h1 className="text-5xl mt-10 flex justify-center">
+          Please get in touch via any of the options below! My inboxes are
+          always open.
+        </h1>
+        <div className="iconWrapper flex flex-row justify-center mt-10">
+          <div className="icon">
+            <a href="mailto:josephsimpsondev@gmail.com">
+              <img src="/images/GmailIcon.png"></img>
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://www.linkedin.com/in/joseph-simpson94/">
+              <img src="/images/LinkedinIcon.png"></img>
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://github.com/joesimp94">
+              <img src="/images/GithubIcon.png"></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ContactMe;
